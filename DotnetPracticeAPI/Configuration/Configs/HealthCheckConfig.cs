e0a59ce9b2e1ec0a525f03f7e6f6400e3cbf9e63
Juan <Jinbyos@Gmail.com>
2025-07-01 02:16:03 -0300

    feat(health): added health checks for redis (production) and redis (development)

--- DotnetPracticeAPI/Configuration/Configs/HealthCheckConfig.cs
+++ DotnetPracticeAPI/Configuration/Configs/HealthCheckConfig.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using StackExchange.Redis;
 
 namespace Configuration.Configs
 {
@@ -6,28 +7,49 @@ namespace Configuration.Configs
     {
         public static IServiceCollection AddHealthCheckConfig (this IServiceCollection services, IConfiguration config, IHostEnvironment host)
         {
-            services.AddHealthChecks()
-                .AddRedis(
-                    redisConnectionString: config.GetConnectionString("RedisConnection")!,
-                    name: "Redis",
-                    tags: ["cache", "redis"],
-                    failureStatus: HealthStatus.Degraded
-                );
 
             if (host.IsDevelopment())
+            {
                 services.AddHealthChecks()
                     .AddSqlServer(
                         connectionString: config.GetConnectionString("DefaultConnection")!,
                         name: "SQL Server",
                         tags: ["db", "sql"]
+                    )
+                    .AddRedis(
+                        redisConnectionString: config.GetConnectionString("RedisConnection")!,
+                        name: "Redis (Development)",
+                        tags: ["cache", "redis"],
+                        failureStatus: HealthStatus.Degraded
                     );
+
+            }
             else
+            {
+                var redisOptions = new ConfigurationOptions
+                {
+                    EndPoints = { "redis-19846.crce181.sa-east-1-2.ec2.redns.redis-cloud.com:19846" },
+                    User = "default",
+                    Password = config["Redis:Password"],
+                    Ssl = true,
+                    AbortOnConnectFail = false
+                };
+
+                var redis = ConnectionMultiplexer.Connect(redisOptions);
+
                 services.AddHealthChecks()
                     .AddNpgSql(
                         connectionString: config.GetConnectionString("DefaultConnection")!,
                         name: "PostgreSQL",
                         tags: ["db", "sql"]
+                    )
+                    .AddRedis(
+                        redis,
+                        name: "Redis (Production)",
+                        tags: ["cache", "redis"],
+                        failureStatus: HealthStatus.Degraded
                     );
+            }
 
             return services;
         }
